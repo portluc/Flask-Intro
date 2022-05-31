00e2537038f0ca80ba1298abe432d576d85c0625
Lucas <lucasmport@gmail.com>
2022-05-31 14:30:31 -0300

    Implementação de funcionalidades CRUD (delete)

--- Flask Intro/app.py
+++ Flask Intro/app.py
@@ -15,7 +15,6 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
-
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
@@ -31,6 +30,17 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
 
 if __name__ == "__main__":
     app.run(debug=True)
